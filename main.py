58a3dfedc977cce715a6ac015dfae61ff2128546
Fleyducc <167763678+Fleyducc@users.noreply.github.com>
2024-09-02 21:29:22 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+
+matn = "Python dasturlash tilida string methodlarini o‘rganamiz."
+
+
+print("Asl matn:")
+print(matn)
+
+upper_matn = matn.upper()
+print("\nUpper usuli yordamida:")
+print(upper_matn)
+
+lower_matn = matn.lower()
+print("\nLower usuli yordamida:")
+print(lower_matn)
+
+capitalize_matn = matn.capitalize()
+print("\nCapitalize usuli yordamida:")
+print(capitalize_matn)
+
+strip_matn = matn.strip()
+print("\nStrip usuli yordamida:")
+print(strip_matn)
+
+title_matn = matn.title()
+print("\nTitle usuli yordamida:")
+print(title_matn)
+
